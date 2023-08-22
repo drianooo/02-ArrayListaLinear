ed45a9c4f4959163a70ea8df32c9610571393341
Adriano Fraga <adriano.rfraga05@gmail.com>
2023-08-22 11:00:17 -0300

    inclusão de busca de elemento na lista

--- ListaLinear/ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear/ListaLinear.cpp
@@ -12,10 +12,12 @@ void buscarElemento();
 //--------------------------
 
 
-const int MAX = 2;;
+const int MAX = 2;
 int lista[MAX]{};
 int nElementos = 0;
-
+int busca;
+int listaBuscar[MAX]{};
+int nElementosBuscar = 0;
 
 int main()
 {
@@ -96,7 +98,7 @@ void inserirElemento()
 		nElementos++;
 	}
 	else {
-		cout << "Lista cheia";
+		cout << "Lista cheia \n";
 	}
 
 }
@@ -104,5 +106,26 @@ void inserirElemento()
 // deve ser implementada como resposta ao exercicio
 void buscarElemento()
 {
-
+	cout << "Digite o elemento que deseja buscar: ";
+	cin >> busca;
+	if (nElementos == 0) {
+		cout << "A lista esta vazia. Nao ha elementos. \n";
+		return;
+	}
+	for (int i = 0; i < nElementos; i++) {
+		if (lista[i]==busca) {
+			listaBuscar[nElementosBuscar] = i;
+			nElementosBuscar++;
+		}
+		if (i == nElementos-1 && nElementosBuscar==0){
+			cout << "Esse elemento nao existe na lista. \n";
+			return;
+		}
+		if (i == nElementos - 1 && nElementosBuscar > 0) {
+			cout << "Esse elemento existe na(s) posicao(s): ";
+			for (int n = 0; n < nElementosBuscar; n++) {
+				cout << listaBuscar[n] << " ";
+			}
+		}
+	}
 }
